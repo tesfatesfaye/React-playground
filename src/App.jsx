43aee27d0c6f7b7a311c3fc60d6e864c8fbfebcb
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-02-05 03:40:31 -0500

    new features discovered

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import './App.css'
-import { useState } from 'react'
+import { useState,useRef,useEffect } from 'react'
 import { DragDropContext,Droppable ,Draggable} from 'react-beautiful-dnd'
 function App() {
+  const app=useRef(null)
+  const[hover,setHover]=useState(false)
+  const [down,setDown]=useState("")
+  const toggleHover=(value)=>{
+    setHover(value)
+    console.log(hover)
+  } 
+   const toggleMouseDown=(value)=>{
+      setDown(value)
+      console.log(value)
+  }
 
+
+  useEffect(()=>{
+    if(hover===false && down==='mousedown'){
+         app.current.click()
+     toggleMouseDown('mouseup')
+    }
+    console.log(hover)
+
+
+  },[hover])
   const cast = [
 
     {
@@ -26,32 +47,41 @@ function App() {
       name: 'Maggie'
     }]
 
+    useEffect(()=>{
+
+    },[])
+
     const [characters,setCharacters]=useState(cast)
     
     const handleOnDragEnd=(result)=>{
-      if(result.destination) return '';
-      console.log(result.destination.index)
-      const items=[...characters]
+      if(!result.destination) return '';
+       const items=[...characters]
       const [holder]=items.splice(result.source.index,1);
       items.splice(result.destination.index,0,holder)
       setCharacters(items)
-    
+      
     }
 
 
   return (
-    <div className="App">
+    <div className="App" ref={app}>
      <h1>The cast of the simpsons</h1>
-     <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId='characters' className='droppable'>
+     <DragDropContext onDragEnd={handleOnDragEnd}   >
+        <Droppable droppableId='characters'  >
           {(provided)=>{
-             return( <ul className='characters'{...provided.droppableProps} ref={provided.innerRef}>
+             return( <ul className='characters'{...provided.droppableProps} ref={provided.innerRef} 
+              
+             onMouseDown={()=>toggleMouseDown('mousedown')} onMouseUp={()=>toggleMouseDown('mouseup')} >
               {characters.map((x,i) => {
 
                 return (
-                <Draggable key={x.id} draggableId={x.id} index={i}>
+                <Draggable key={x.id} draggableId={x.id} index={i}  isDragDisabled={hover ? false : true}>
                   {(prov)=>(
-                        <li className='cast'{...prov.draggableProps} {...prov.dragHandleProps} ref={prov.innerRef} >
+                  
+                        <li className='cast'{...prov.draggableProps} {...prov.dragHandleProps} ref={prov.innerRef} 
+                       
+                        >
+                          <div className='divxx' onMouseEnter={()=>toggleHover(true)}  onMouseLeave={()=>toggleHover(false)}> xx</div>
                      {x.name}
                      </li>
 
