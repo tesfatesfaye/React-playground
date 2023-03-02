185dd57b35ecdfe05b3372efcd69016ebb6ca44b
Tesfa Tesfaye <tesfaget15@gmail.com>
2023-03-02 13:36:25 -0500

    first commit

---
+++ CustomUtilites/useAutosizeText.jsx
@@ -0,0 +1,29 @@
+import {useContext,useState,useLayoutEffect, useRef, useEffect} from "react";
+
+const useAutoSizeText=()=>{
+const[tasks,setTasks]=useState()
+let descriptionRef=useRef(null)
+useLayoutEffect(()=>{
+ 
+    if(descriptionRef.current){
+        console.log(descriptionRef.current.style.height)
+       descriptionRef.current.style.height="19px"
+        const scrollH=descriptionRef.current.scrollHeight;
+        descriptionRef.current.style.height=scrollH+"px"     
+        console.log(descriptionRef.current.style.height)
+    }
+    else{
+        console.log("No joy")
+    }
+     return()=>{
+          
+        console.log('cleanedUp')
+        descriptionRef=null
+          
+     }
+},[tempHolder])
+    return{descriptionRef}
+
+}
+
+export default useAutoSizeText
